8c606493d23131eee0223b470f0fdd5161a1433d
Breno Pereira <brenomartinsxd@gmail.coma>
2020-05-31 22:55:04 -0400

    Change structure for DDD

--- app/ExceptionHandler.php
+++ app/ExceptionHandler.php
@@ -2,10 +2,10 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Foundation\Exceptions\Handler;
 use Throwable;
 
-class Handler extends ExceptionHandler
+class ExceptionHandler extends Handler
 {
     /**
      * A list of the exception types that are not reported.
